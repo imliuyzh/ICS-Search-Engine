78da8ddfd35dfbdd8c85faf7728cfd9318faad46
Onion <thesneakyonion@rape.lol>
2020-02-24 12:23:30 -0800

    re.compile global for efficiency(?)

--- indexer.py
+++ indexer.py
@@ -1,17 +1,17 @@
-import json, re, pickle
+import json, re, pickle, datetime
 from collections import defaultdict
 from os import listdir
 from os.path import isfile, join
 from os import walk
 from bs4 import BeautifulSoup
 from nltk import PorterStemmer
-import datetime
 from sys import getsizeof
 
-#implement
+
+tokenMatch = re.compile(r"[a-z0-9]+")
 def tokenize(text: str) -> [str]:
     """Take a string and break it down to a list of alphanumeric sequences."""
-    return re.findall(r"[a-z0-9]+", text.lower())
+    return re.findall(tokenMatch, text.lower())
 
 def removeNoise(documentTree: BeautifulSoup) -> None:
     """Clear all the data noise from the given document tree."""
@@ -21,43 +21,42 @@ def removeNoise(documentTree: BeautifulSoup) -> None:
 def parse(toParse: dict) -> ([str], [str]):
     """Given a JSON dict and parse it into lists of tokens."""
     soup = BeautifulSoup(toParse['content'], 'lxml')
-    
+
     importantCandidate = soup.findAll(["title", "strong", "b", "h1",
         "h2", "h3", "h4", "h5", "h6"])
-    importantToken = []
+    importantTokens = []
     for x in importantCandidate:
-        importantToken.extend(tokenize(x.extract().get_text()))
-    
-    removeNoise(soup)
-    normalToken = tokenize(soup.get_text())
-    return importantToken, normalToken
+        importantTokens.extend(tokenize(x.extract().get_text()))
 
-def writeToIndex(new):
+    removeNoise(soup)
+    normalTokens = tokenize(soup.get_text())
+    return importantTokens, normalTokens
+
+def writeToIndex(new: dict) -> None:
     """Dumps >10MB index to old index"""
     old = pickle.load(open("index.p", "rb"))
-    for key, set in new.items():
-        old[key] = old[key].union(set)
+    for key, ids in new.items():
+        old[key] = old[key].union(ids)
     pickle.dump(old, open("index.p", "wb"))
     new.clear()
 
-def index():
+def index() -> None:
     n = 0 # docid
     i = defaultdict(set) #tokens
     pickle.dump(i, open("index.p", "wb"))
     ps = PorterStemmer()
 
-    #start time
+    # start time
     print("start time: {0}".format(datetime.datetime.now()))
 
     # From Stack Overflow
-    # below line has old small documents option
-    #for file_name in [f for f in listdir("./documents") if isfile(join("./documents", f))]:
-    for dirpath, _, files in walk("./documents", topdown=False):
+
+    for indexerPath, _, files in walk("./DEV", topdown=False):
         for file_name in files:
-            with open(join(dirpath, file_name)) as jsonFile:
+            with open(join(indexerPath, file_name)) as jsonFile:
                 n = n + 1
-                importantToken, normalToken = parse(json.load(jsonFile))
-                for token in importantToken + normalToken:
+                importantTokens, normalTokens = parse(json.load(jsonFile))
+                for token in importantTokens + normalTokens:
                     i[ps.stem(token)].add(n)
                 if getsizeof(i) > 10000000:
                     writeToIndex(i)
@@ -71,6 +70,7 @@ def index():
 
     #end time
     print("end time: {0}".format(datetime.datetime.now()))
+
 
 if __name__ == '__main__':
     index()
