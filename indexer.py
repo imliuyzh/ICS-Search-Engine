c78d9b4396cee710917ccb279fae4fd52c9752d4
Yizhen Liu <imliuyzh@outlook.com>
2020-02-23 19:12:20 -0800

    Improve comments

--- indexer.py
+++ indexer.py
@@ -1,16 +1,14 @@
-import json, re
+import json, re, pickle
 from collections import defaultdict
 from bs4 import BeautifulSoup
 from os import listdir
 from os.path import isfile, join
-import pickle
 
 
 #implement
 def tokenize(text: str) -> [str]:
     """Take a string and break it down to a list of alphanumeric sequences."""
     return re.findall(r"[a-z0-9]+", text.lower())
-    #return re.sub(r'[^a-z0-9]', " ", text.lower()).split()
 
 def removeNoise(documentTree: BeautifulSoup) -> None:
     """Clear all the data noise from the given document tree."""
@@ -18,14 +16,17 @@ def removeNoise(documentTree: BeautifulSoup) -> None:
         x.extract()
 
 def parse(toParse: dict) -> ([str], [str]):
-    """Given a HTML string and parse it into a list of tokens."""
+    """Given a HTML string and parse it into lists of tokens."""
     soup = BeautifulSoup(toParse['content'], 'lxml')
-    importantToken = []
-    for x in soup.findAll([
-        "title", "strong", "b", "h1", "h2", "h3", "h4", "h5", "h6"]):
+    
+    importantToken = []     # Bolded words, headings, title...
+    importantCandidate = soup.findAll(["title", "strong", "b", "h1", "h2",
+        "h3", "h4", "h5", "h6"])
+    for x in importantCandidate:
         importantToken.extend(tokenize(x.extract().get_text()))
+    
     removeNoise(soup)
-    normalToken = tokenize(soup.get_text())
+    normalToken = tokenize(soup.get_text())     # Words other than those above
     return importantToken, normalToken
 
 def index():
