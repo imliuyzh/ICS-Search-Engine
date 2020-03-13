abb991af2a5a7c659a15e1d2b554dc4f3cdf5a00
Nathaniel Nebel <me@nathanielnebel.com>
2020-03-12 22:45:02 -0700

    add caching

--- search.py
+++ search.py
@@ -27,28 +27,27 @@ FILE = 2
 
 MODE = ORIGINAL
 
-CACHE = {}
+class cache:
+    store = []
+    store_dict = {}
+
+    @classmethod
+    def get_index(cls, token):
+        if token[0] in cache.store:
+            return cache.store_dict[token[0]][ps.stem(token)]
+        else:
+            p = pickle.load(open("./index/{}.p".format(token[0]), 'rb'))
+            cache.store.append(token[0])
+            cache.store_dict[token[0]] = p
+
+            if len(cache.store) > 3:
+                to_remove = cache.store.pop(0)
+                del cache.store_dict[to_remove]
+
+            return p[ps.stem(token)]
 
 def get_token_info(token: str) -> dict: # {term: {docID: (important, count)}
-    p = pickle.load(open("./index/{}.p".format(token[0]), 'rb'))
-    return p[ps.stem(token)]
-
-def get_term_info_dict_version(term: str) -> dict:
-    with open("./index/{}.p".format(term[0]), 'rb') as p:
-        index_dict = pickle.load(p)
-        return index_dict[term]
-
-def get_term_info_file_version(term: str, open_index_files: dict) -> dict:
-    term_info_file = open_index_files[term[0]]
-    term_info_file.seek(term_indices[term])
-    term_info = term_info_file.readline()
-    term_info_list = term_info.split(',')
-    term_info = {}
-    for i in range(1, (len(term_info_list) - 3)):
-        term_info[term_info_list[i]] = (term_info_list[i+1], term_info_list[i+2])
-    return term_info
-
-
+    return cache.get_index(token)
 
 def search(userIn: str) -> [int]:
     tokens = set(ps.stem(token) for token in (i for i in userIn.lower().split() if i not in stop_words))
@@ -62,7 +61,7 @@ def get_tf_idf_list(terms: set) -> [int]:
 
     term_dict = dict()
     for term in terms:
-        term_dict[term] = get_term_info_dict_version(term)
+        term_dict[term] = get_token_info(term)
 
     def find_document(doc_ids: set) -> dict:
         document_dict = {doc_id:0 for doc_id in doc_ids}
