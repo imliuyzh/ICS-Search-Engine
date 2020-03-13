6127a9f9f9985eef7274dbefed836e0daa164b3a
sseader <szseader@gmail.com>
2020-03-12 23:38:01 -0700

    splitter_2 finished, index files finished, query finished

--- index_splitter_2.py
+++ index_splitter_2.py
@@ -3,39 +3,53 @@ from string import ascii_lowercase
 import os
 
 if __name__ == "__main__":
+
     intStrs = "0123456789"
-    full_alpha = ascii_lowercase + intStrs
+    full_alpha = intStrs + ascii_lowercase
     file_list = []
     file_indices = {}
     try:
         os.mkdir("index2")
     except(FileExistsError):
         pass
+
     for character in full_alpha:
-        open("index2/" + character + ".txt", 'w').close()
+        open("index2/" + character + ".txt", 'wb').close()
         file_list.append("index2/" + character + ".txt")
     with open("index.p", 'rb') as index_pickle:
         index = pickle.load(index_pickle)
     current_character_index = 0
     current_character = full_alpha[current_character_index]
     running_total_in_file = 0
-    current_file = open(file_list[current_character_index], 'w')
-
+    current_file = open(file_list[current_character_index], 'wb')
     for term in sorted(index.keys()):
         while term[0] != full_alpha[current_character_index]:
             current_character_index += 1
-        if current_character != full_alpha[current_character_index]:
+        if  current_character != full_alpha[current_character_index]:
             running_total_in_file = 0
             current_character = full_alpha[current_character_index]
             current_file.close()
-            current_file = open(file_list[current_character_index], 'w')
+            current_file = open(file_list[current_character_index], 'wb')
 
-        current_file.write(term + ',')
+        file_indices[term] = running_total_in_file
+        current_file.write((term + ',').encode('utf-8'))
         for key in index[term].keys():
-            current_file.write(str(key) + ',')
+            current_file.write((str(key) + ',').encode('utf-8'))
             for val in index[term][key]:
-                current_file.write(str(val) + ',')
-        current_file.write('\n')
+                current_file.write((str(val) + ',').encode('utf-8'))
+        current_file.write('\n'.encode('utf-8'))
         running_total_in_file += 1
 
+    for file in file_list:
+        position = 0
+        print(file)
+        with open(file, 'r') as f:
 
+            line = f.readline()
+            while line != '':
+                file_indices[line.split(',')[0]] = position
+                position = f.tell()
+                line = f.readline()
+
+
+    pickle.dump(file_indices, open("file_indices.p", 'wb'))
--- search.py
+++ search.py
@@ -20,7 +20,10 @@ term_freqs = dict()
 id_freqs = dict()
 stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
 
-term_indices = {} #pickle.load(open("term_indices.p", 'rb'))
+term_indices = pickle.load(open("file_indices.p", 'rb'))
+open_files = {}
+for f in "0123456789abcdefghijklmnopqrstuvwxyz":
+    open_files[f] = open("index2/{}.txt".format(f), 'r')
 ORIGINAL = 0
 DICT = 1
 FILE = 2
@@ -36,16 +39,21 @@ def get_token_info(token: str) -> dict: # {term: {docID: (important, count)}
 def get_term_info_dict_version(term: str) -> dict:
     with open("./index/{}.p".format(term[0]), 'rb') as p:
         index_dict = pickle.load(p)
-        return index_dict[term]
+        try:
+            return index_dict[term]
+        except KeyError:
+            return
 
-def get_term_info_file_version(term: str, open_index_files: dict) -> dict:
-    term_info_file = open_index_files[term[0]]
+def get_term_info_file_version(term: str) -> dict:
+    term_info_file = open_files[term[0]]
     term_info_file.seek(term_indices[term])
     term_info = term_info_file.readline()
+    print(term_info)
     term_info_list = term_info.split(',')
+    print(term_info_list)
     term_info = {}
-    for i in range(1, (len(term_info_list) - 3)):
-        term_info[term_info_list[i]] = (term_info_list[i+1], term_info_list[i+2])
+    for i in range(1, (len(term_info_list) - 4), 3):
+        term_info[int(term_info_list[i])] = (bool(term_info_list[i+1]), int(term_info_list[i+2]))
     return term_info
 
 
@@ -62,7 +70,7 @@ def get_tf_idf_list(terms: set) -> [int]:
 
     term_dict = dict()
     for term in terms:
-        term_dict[term] = get_term_info_dict_version(term)
+        term_dict[term] = get_term_info_file_version(term)
 
     def find_document(doc_ids: set) -> dict:
         document_dict = {doc_id:0 for doc_id in doc_ids}
@@ -83,8 +91,10 @@ def get_tf_idf_list(terms: set) -> [int]:
     tf_idf_dict = find_document(doc_set)
     for docid in tf_idf_dict:
         for term in terms:
-            tf, idf = (term_dict[term][docid][1] / idmap[docid][1]), (math.log(n / len(term_dict[term].keys())))
-            tf_idf_dict[docid] += (tf * idf)
+            tf = term_dict[term][docid][1]
+            oth = idmap[docid][1]
+            idf = (math.log(n / len(term_dict[term].keys())))
+            tf_idf_dict[docid] += (tf/oth * idf)
     return sorted(tf_idf_dict, key=lambda x: -tf_idf_dict[x])
 
 # index = {term: {docID: (important, count)}}
