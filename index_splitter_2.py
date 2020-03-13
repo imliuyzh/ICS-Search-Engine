f7395bf5ef217abb59d2968358ca475dfb172612
sseader <szseader@gmail.com>
2020-03-12 21:54:51 -0700

    mostly finished splitter_2

--- index_splitter_2.py
+++ index_splitter_2.py
@@ -19,11 +19,11 @@ if __name__ == "__main__":
     current_character_index = 0
     current_character = full_alpha[current_character_index]
     running_total_in_file = 0
+    current_file = open(file_list[current_character_index], 'w')
 
     for term in sorted(index.keys()):
         while term[0] != full_alpha[current_character_index]:
             current_character_index += 1
-        current_file = open(file_list[current_character_index], 'w')
         if current_character != full_alpha[current_character_index]:
             running_total_in_file = 0
             current_character = full_alpha[current_character_index]
@@ -32,16 +32,10 @@ if __name__ == "__main__":
 
         current_file.write(term + ',')
         for key in index[term].keys():
-            print(index[term])
-            print(key)
             current_file.write(str(key) + ',')
             for val in index[term][key]:
                 current_file.write(str(val) + ',')
-        current_file.write('/n')
+        current_file.write('\n')
         running_total_in_file += 1
-        if running_total_in_file > 20:
-            break
-
-        #file_indices[file_list[current_character_index]]
 
 
